f2aa239f1ce1109ad7ac94d4d33c3cfffa349ef7
Rafael Nami <arktech@arktechconsulting.com.br>
2013-05-19 16:49:03 +1200

    Adding bower packaging

---
+++ index-web.js
@@ -0,0 +1,3 @@
+module.exports = require('./lib/persistence.js');
+module.exports = require('./lib/persistence.store.sql.js');
+module.exports = require('./lib/persistence.store.persistence.store.websql.js');
